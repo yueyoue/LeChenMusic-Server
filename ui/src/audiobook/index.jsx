9925dc9caa7c9e7a9592dd8d8fefec4ef219cb68
yueyoue <165250587+yueyoue@users.noreply.github.com>
2026-07-08 16:37:13 +0800

    feat: export new audiobook components

--- ui/src/audiobook/index.jsx
+++ ui/src/audiobook/index.jsx
@@ -1,4 +1,7 @@
 import AudiobookList from './AudiobookList'
+import AudiobookDetail from './AudiobookDetail'
+import AudiobookPlayer from './AudiobookPlayer'
+import NarratorList from './NarratorList'
 import React from 'react'
 
 const AudiobookIcon = () => <span style={{ fontSize: 20 }}>📖</span>
@@ -13,4 +16,5 @@ const admin = {
   ...all,
 }
 
+export { AudiobookDetail, AudiobookPlayer, NarratorList }
 export default { all, admin }
