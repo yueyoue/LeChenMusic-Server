c19b562b839d4011ab5f624400f0c620e4bffec7
yueyoue <165250587+yueyoue@users.noreply.github.com>
2026-07-08 16:36:57 +0800

    feat: add audiobook detail and narrator routes

--- ui/src/routes.jsx
+++ ui/src/routes.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Personal from './personal/Personal'
 import AudiobookList from './audiobook/AudiobookList'
+import AudiobookDetail from './audiobook/AudiobookDetail'
+import NarratorList from './audiobook/NarratorList'
 
 const routes = [
   <Route exact path="/personal" render={() => <Personal />} key={'personal'} />,
+  <Route exact path="/audiobook" render={() => <AudiobookList />} key={'audiobook'} />,
   <Route exact path="/audiobook/starred" render={() => <AudiobookList />} key={'audiobook-starred'} />,
-  <Route exact path="/narrator" render={() => <div style={{padding: 20}}><h2>🎤 演播者</h2><p>演播者页面开发中...</p></div>} key={'narrator'} />,
+  <Route exact path="/audiobook/:id" render={({ match }) => <AudiobookDetail id={match.params.id} />} key={'audiobook-detail'} />,
+  <Route exact path="/narrator" render={() => <NarratorList />} key={'narrator'} />,
 ]
 
 export default routes
